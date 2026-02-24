2e25ba26c643e6108fc7bd1e40375d41385e63a2
AshwiniBorse17 <ashwini.borse@kylas.io>
2026-02-24 13:33:25 +0530

    Added complete Cars App project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Cars from './components/Cars';
+
+function App() {
+  return (
+    <div className="App">
+      <Cars></Cars>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewCars.js
@@ -0,0 +1,19 @@
+import './NewCars.css';
+
+function NewCars(){
+    return(
+        <div className="newcars">
+            <h1>New Cars</h1>
+            <div className="newcars-list">
+                <img src="Newcar1.jpg" alt="car1" width="350px" height="200px"></img>
+                <img src="newcar2.jpg" alt="car2" width="350px" height="200px"></img>
+                <img src="newcar3.jpg" alt="car3" width="350px" height="200px"></img>
+                <img src="newcar4.jpg" alt="car4" width="350px" height="200px"></img>
+                <img src="newcar5.jpg" alt="car5" width="350px" height="200px"></img>
+                <img src="newcar6.jpg" alt="car6" width="350px" height="200px"></img>
+            </div>
+        </div>   
+        );
+}
+
+export default NewCars;
